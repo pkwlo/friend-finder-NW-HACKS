2a59ae7ac7065feacd72a3fefb135612f64ff669
Alice Huang <alice.in.realityland@gmail.com>
2024-01-20 15:33:55 -0800

    Add read and write for questionnaire

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,29 @@ $(document).ready(function () {
     checkQuestionnaire();
 });
 
-function checkQuestionnaire() {
-    firebase.auth().onAuthStateChanged(function (user) {
+var user_tags = [];
+
+async function checkQuestionnaire() {
+    firebase.auth().onAuthStateChanged(async function (user) {
         if (user) {
-            db.collection("questionnaire").where("user_id", "==", user.uid).get().then(function (querySnapshot) {
-                if (querySnapshot.size <= 0) {
-                    
+            await db.collection("questionnaire").where("user_id", "==", user.uid).get().then(
+                results => {
+                    results.forEach(result => {
+                        user_tags.push(result.data().interests);
+                        user_tags.push(result.data().hangout);
+                        user_tags.push(result.data().chat);
+                        user_tags.push(result.data().outdoor);
+                        user_tags.push(result.data().location);
+                    })
+                }
+            )
+            .then(() => {
+                if (user_tags.length == 0) {
                     window.location.assign("questionnaire.html");
                 }
             })
+            console.log(user_tags);
         }
     })
+    
 }

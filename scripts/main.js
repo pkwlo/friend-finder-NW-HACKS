6b16b7a6fbaf30d1436235a61d611a86d06f0634
Patricia Lo <pkwlo1992@gmail.com>
2024-01-20 17:10:38 -0800

    add matching system and linked to firebase

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
     checkQuestionnaire();
+    
 });
 
 var user_tags = [];
+var user_interests = [];
 
 async function checkQuestionnaire() {
     firebase.auth().onAuthStateChanged(async function (user) {
@@ -10,7 +12,7 @@ async function checkQuestionnaire() {
             await db.collection("questionnaire").where("user_id", "==", user.uid).get().then(
                 results => {
                     results.forEach(result => {
-                        user_tags.push(result.data().interests);
+                        user_interests.push(result.data().interests);
                         user_tags.push(result.data().hangout);
                         user_tags.push(result.data().chat);
                         user_tags.push(result.data().outdoor);
@@ -23,8 +25,50 @@ async function checkQuestionnaire() {
                     window.location.assign("questionnaire.html");
                 }
             })
-            console.log(user_tags);
+            // console.log(user_tags);
+            matchUsers(user_tags, user_interests);
         }
     })
     
+}
+
+//Loop through all users and compare tags and give each user a score according to how many tags they have in common with the current user
+function matchUsers(user_tags, user_interests) {
+    db.collection("questionnaire").get().then(
+        allUsers => {
+            allUsers.forEach(user => {
+                var score = 0;
+                var next_user_tags = [];
+                var next_user_interests = [];
+                var other_user_id = user.data().user_id;
+                var current_user_id = firebase.auth().currentUser.uid;
+
+                if (user.data().user_id != firebase.auth().currentUser.uid) {
+                    next_user_interests.push(user.data().interests);
+                    next_user_tags.push(user.data().hangout);
+                    next_user_tags.push(user.data().chat);
+                    next_user_tags.push(user.data().outdoor);
+                    next_user_tags.push(user.data().location);
+
+                    for (var i = 0; i < user_tags.length; i++) {
+                        if (user_tags[i] == next_user_tags[i]) {
+                            score++;
+                        }
+                    }
+    
+                    for (var i = 0; i < user_interests[0].length; i++) {
+                        if (next_user_interests[0].includes(user_interests[0][i])) {
+                            score++;
+                        }
+                    }
+                    console.log(score);
+
+                    //add the score to the user's document in the database
+                    db.collection("users").doc(current_user_id).collection("other_users").add(
+                        {   name: other_user_id,
+                            score: score
+                        })
+                }
+            })
+        })
 }

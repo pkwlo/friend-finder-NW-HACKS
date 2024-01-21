4b3de79ab1ff545c92ac4e2ac2f63dc43342fede
Alice Huang <alice.in.realityland@gmail.com>
2024-01-20 17:36:55 -0800

    Finish basic logic of sending and receiving messages

---
+++ scripts/chatroom.js
@@ -0,0 +1,57 @@
+let username
+
+$(document).ready(async function () {
+    firebase.auth().onAuthStateChanged(async function (user) {
+        if (user) {
+            await db.collection("users").doc(user.uid).get().then(
+                results => {
+                    username = results.data().name;
+
+                }
+            )
+        }
+    })
+    loadMessages();
+});
+
+async function loadMessages() {
+    var messages = document.getElementById("chats-go-here");
+    await db.collection("chatrooms").doc('0B6f234PRY9UrdadngOZ').collection("message_history").onSnapshot(
+        chats => {
+            console.log('here')
+            chats.docChanges().forEach(chat => {
+                if (chat.type == "added") {
+                    var message_card = document.createElement("div");
+                    var message = document.createElement("div");
+                    message.innerHTML = chat.doc.data().message;
+                    var time = document.createElement("div");
+
+                    // fix this
+                    time.innerHTML = chat.doc.data().time.toDate();
+
+                    var displayname = document.createElement("div");
+                    displayname.innerHTML = chat.doc.data().username;
+                    message_card.appendChild(displayname);
+                    message_card.appendChild(message);
+                    message_card.appendChild(time);
+                    messages.appendChild(message_card);
+        }})
+        
+})
+}
+
+function sendMessage() {
+    var message = document.getElementById("chat-input").value;
+
+    // db.collection("chatrooms").doc('0B6f234PRY9UrdadngOZ').update({
+    //     message_history: firebase.firestore.FieldValue.arrayUnion({
+    //         message: message,
+    //     })
+    // })
+
+    db.collection("chatrooms").doc('0B6f234PRY9UrdadngOZ').collection("message_history").add({
+        message: message,
+        time: firebase.firestore.Timestamp.fromDate(new Date()),
+        username: username
+    })
+}
